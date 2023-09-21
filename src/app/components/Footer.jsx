4e66bb1603fe907d675653249aa43cd1301db906
oma-umeh <helenumeh21@gmail.com>
2023-09-21 17:02:48 +0100

    fix: slug error resolved

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -50,13 +50,13 @@ export default function Footer() {
             <BsArrowRight />
          </div>
 
-          <div className="flex justify-center py-4 gap-2 text-2xl text-slate-900">
-          <AiOutlineFacebook  className="bg-white rounded-full p-1 hover:text-blue-700"/>
-          <AiOutlineInstagram  className="bg-white rounded-full p-1 hover:text-blue-700"/>
-          <AiOutlineTwitter  className="bg-white rounded-full p-1 hover:text-blue-700"/>
-          <AiOutlineYoutube  className="bg-white rounded-full p-1 hover:text-blue-700"/>
-          <AiOutlineGooglePlus  className="bg-white rounded-full p-1 hover:text-blue-700"/>
-          <AiOutlineLinkedin className="bg-white rounded-full p-1 hover:text-blue-700" />
+          <div className="flex justify-center py-4 gap-2 text-2xl text-blue-700">
+          <AiOutlineFacebook  className="bg-white rounded-full p-1  hover:text-blue-900"/>
+          <AiOutlineInstagram  className="bg-white rounded-full p-1  hover:text-blue-900"/>
+          <AiOutlineTwitter  className="bg-white rounded-full p-1  hover:text-blue-900"/>
+          <AiOutlineYoutube  className="bg-white rounded-full p-1  hover:text-blue-900"/>
+          <AiOutlineGooglePlus  className="bg-white rounded-full p-1  hover:text-blue-900"/>
+          <AiOutlineLinkedin className="bg-white rounded-full p-1  hover:text-blue-900" />
           </div>
         </div>
         </section>
--- src/app/recipe/[slugs]/page.jsx
+++ src/app/recipe/[slugs]/page.jsx
@@ -1,28 +1,23 @@
 "use client"
-import { data } from '@/app/page'
 import Image from 'next/image'
+import {recipeData} from '../../Shared'
 
 
 
+export default function page({ params }) {
 
-export default function page({params}) {
+  const recipe = recipeData.find(pro => pro.id == params.slugs)
 
-    const recipe = data.find(pro => pro.id === +(params.slugs))
-    console.log(params);
-    console.log({recipe});
-    console.log(data);
-   
-    
+
   return (
     <section className='flex  items-center justify-center p-40 '>
-        <div className='flex flex-col items-center justify-center text-lg text-red-950 font-bold p-10 gap-6'>
+      <div className='flex flex-col items-center justify-center text-lg text-red-950 font-bold p-10 gap-6'>
         <h1 className='text-2xl font-extrabold'>{recipe.name}</h1>
-        <Image src={recipe.image} alt={recipe.name} width={300} height={300} className='h-[300px] w-[300px] rounded-md'/>
+        <Image src={recipe.image} alt={recipe.name} width={300} height={300} className='h-[300px] w-[300px] rounded-md' />
         <p>Category: {recipe.type}</p>
         <p>Ingredients: {recipe.ingredients}</p>
         <p>Instruction: {recipe.instructions}</p>
-
-        </div>
+      </div>
     </section>
   )
 }
