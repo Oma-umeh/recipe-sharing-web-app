62792542f5e8a5225251e4fc49ddce6ccdaab528
oma-umeh <helenumeh21@gmail.com>
2023-09-30 10:58:20 +0100

    fix: edited a comment on the contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,7 +4,7 @@ export default function Page() {
     <section className='flex flex-col md:flex-row p-4 md:p-[102px] text-red-950 bg-slate-50'>
       <div className='md:w-1/2 p-4'>
         <h1 className='text-4xl md:text-6xl font-bold py-5'>Contact Us</h1>
-        <h3 className='text-xl font-semibold px-2'>Get In Touch</h3>
+        <h3 className='text-xl font-semibold px-2'>Give us a feedback to help us serve you better</h3>
         <div className='text-lg px-2 py-3'>
           <p><span className='font-bold'>Send us a mail:</span><a href="mailto:myrecipe@gmail.com" className="text-sm hover:text-blue-700"> myrecipe@gmail.com</a></p>
           <p><span className='font-bold'>Give us a call:</span><a href="tel:+2341234567890" className="text-sm hover:text-blue-700"> +234 1234567890</a> </p>
